7525083d607392b3e4d7b7d4f20b2e9944fed265
Saksham Agarwal <sak9agar@yahoo.co.in>
2016-01-07 22:33:51 +0530

    Added car class

---
+++ Car.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Car {
+	
+	int numberOfSeats;
+
+	public Car(int wheels, String reg, int miles, int seats) {
+		super(wheels, reg, miles);
+		numberOfSeats = seats;
+	}
+
+	
+}

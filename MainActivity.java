6d1c2f8772a304f9d00ec0d1bee9ed884d7a4160
Saksham Agarwal <sak9agar@yahoo.co.in>
2016-01-23 02:29:46 +0530

    Calculator with single onClickHandler

--- MainActivity.java
+++ MainActivity.java
@@ -9,24 +9,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView result;
+    EditText input1, input2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView result = (TextView)findViewById(R.id.result);
-        final EditText input1 = (EditText)findViewById(R.id.input1);
-        final EditText input2 = (EditText)findViewById(R.id.input2);
-
-        Button plus = (Button)findViewById(R.id.plus);
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int a = Integer.parseInt(input1.getText().toString());
-                int b = Integer.parseInt(input2.getText().toString());
-                result.setText(Integer.toString(a + b));
-            }
-        });
+        input1 = (EditText)findViewById(R.id.input1);
+        input2 = (EditText)findViewById(R.id.input2);
+        result = (TextView)findViewById(R.id.result);
     }
 
 
+    public void calculate(View view) {
+        int a = Integer.parseInt(input1.getText().toString());
+        int b = Integer.parseInt(input2.getText().toString());
+
+        switch (view.getId()) {
+            case R.id.plus:
+                result.setText(Integer.toString(a + b));
+                break;
+
+            case R.id.minus:
+                result.setText(Integer.toString(a - b));
+                break;
+            case R.id.mul:
+                result.setText(Integer.toString(a * b));
+                break;
+            case R.id.div:
+                result.setText(Integer.toString(a / b));
+                break;
+        }
+    }
 }
